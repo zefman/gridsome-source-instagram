872a32eb918091af0fb4c0aae685112529f3d8ce
Jozef Maxted <jozef.maxted@stratumfive.com>
2020-06-04 20:10:47 +0100

    Use puppeteer to load instragram data

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const { getInstagramPhotos } = require('./src/instagram');
 
+// eslint-disable-next-line func-names
 module.exports = function (api, options) {
-
   if (!options.username) {
     throw new Error('Missing Instagram username');
   }
+
+  if (!options.password) {
+    throw new Error('Missing Instagram password');
+  }
+
   api.loadSource(async ({ addCollection }) => {
     const contentType = addCollection({
-      typeName: options.typeName
+      typeName: options.typeName,
     });
 
     try {
-      const photos = await getInstagramPhotos(options.username);
-      photos.forEach(photo => {
+      const photos = await getInstagramPhotos(options.username, options.password);
+      photos.forEach((photo) => {
         contentType.addNode(photo);
       });
     } catch (error) {
@@ -23,5 +28,5 @@ module.exports = function (api, options) {
 };
 
 module.exports.defaultOptions = () => ({
-  typeName: 'InstagramPhoto'
+  typeName: 'InstagramPhoto',
 });
--- src/instagram.js
+++ src/instagram.js
@@ -1,30 +1,42 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 const INSTAGRAM_URL = 'https://www.instagram.com/';
 
 /**
  * Get a user's Istagram photos
  * @param {string} username The instagram username
+ * @param {string} password The instagram password
  */
-async function getInstagramPhotos(username) {
-  const photos = await axios
-    .get(`https://www.instagram.com/${username}/?__a=1`)
-    .then(response => {
-      const photos = []
-      console.log(response.data);
-      response.data.graphql.user.edge_owner_to_timeline_media.edges.forEach(
-        edge => {
-          if (edge.node) {
-            photos.push(edge.node)
-          }
-        }
-      );
-      return photos;
-    });
+async function getInstagramPhotos(username, password) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://www.instagram.com/');
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', username);
+  await page.type('input[name="password"]', password);
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+  await page.goto(`https://www.instagram.com/${username}/?__a=1`);
+
+  const content = await page.content();
+  console.log(content);
+  const $ = cheerio.load(content);
+  const data = JSON.parse($('pre').text());
+
+  const photos = [];
+  data.graphql.user.edge_owner_to_timeline_media.edges.forEach(
+    (edge) => {
+      if (edge.node) {
+        photos.push(edge.node);
+      }
+    },
+  );
 
   return photos;
 }
+
 module.exports = {
   getInstagramPhotos,
 };
